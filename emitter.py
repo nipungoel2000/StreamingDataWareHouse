0a6ea5d616e682d097a2d81815e1fcb757fb2c74
nipungoel2000 <nipungoel350@gmail.com>
2022-04-08 12:17:55 +0530

    Emiiter modified: made to emit valid data as per primary key

--- emitter.py
+++ emitter.py
@@ -2,24 +2,35 @@ import time
 import random
 import csv
 
-SLEEP_TIME = 5
+SLEEP_TIME = 1
 EMITTER_LOC = "fact_data_emitter/"
 
-def emitter(cnt):
-    storeId = random.randint(1,4)
-    productId = random.randint(1,5)
-    purchaseAmount = random.randint(1,5000)
-    data = [storeId, productId, purchaseAmount]
-    # data = [{"storeId" : storeId, "productId" : productId, "purchaseAmount" : purchaseAmount}]
-    fileName = EMITTER_LOC + str(cnt) + ".csv"
-    with open(fileName, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(data)
-
-if __name__=="__main__":
+def emitter():
     cnt = 1
+    lst = []
     while True:
+        storeId = random.randint(1,4)
+        productId = random.randint(1,5)
+        while((storeId,productId) in lst):
+            storeId = random.randint(1,4)
+            productId = random.randint(1,5)
+        lst.append((storeId,productId))
+        purchaseAmount = random.randint(1,5000)
+        data = [storeId, productId, purchaseAmount]
+        # print(data)
+        # data = [{"storeId" : storeId, "productId" : productId, "purchaseAmount" : purchaseAmount}]
+        fileName = EMITTER_LOC + str(cnt) + ".csv"
+        with open(fileName, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
         print(str(cnt) + " file created..")
-        emitter(cnt)
         cnt += 1
         time.sleep(SLEEP_TIME)
+
+if __name__=="__main__":
+    # cnt = 1
+    # while True:
+    # print(str(cnt) + " file created..")
+    emitter()
+        # cnt += 1
+        # time.sleep(SLEEP_TIME)
